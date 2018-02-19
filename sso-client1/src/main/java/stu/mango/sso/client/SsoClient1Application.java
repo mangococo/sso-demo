4672d5a598eb0bd507778252d6184f6d95729950
MangoZ <blackocean1127@gmail.com>
2018-02-19 16:36:48 +0800

    源码提交

---
+++ sso-client1/src/main/java/stu/mango/sso/client/SsoClient1Application.java
@@ -0,0 +1,23 @@
+package stu.mango.sso.client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class SsoClient1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoClient1Application.class, args);
+    }
+
+    @GetMapping("/user")
+    public Object user(Authentication authentication) {
+        return authentication;
+    }
+}
