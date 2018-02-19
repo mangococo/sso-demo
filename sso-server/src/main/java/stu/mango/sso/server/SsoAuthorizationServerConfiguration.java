65249facc9b6b326b70c45c0d0705e4233a565bf
MangoZ <blackocean1127@gmail.com>
2018-02-19 16:02:23 +0800

    源码提交

---
+++ sso-server/src/main/java/stu/mango/sso/server/SsoAuthorizationServerConfiguration.java
@@ -0,0 +1,59 @@
+package stu.mango.sso.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class SsoAuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client1")
+                .secret("secret1")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .scopes("all")
+                .and()
+                .withClient("client2")
+                .secret("secret2")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .scopes("all");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(jwtTokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        /*
+         授权表达式，当访问授权服务器的tokenKey（签名密钥 SigningKey）时需要进行身份认证
+         默认为 denyAll()，拒绝所有
+          */
+
+        security.tokenKeyAccess("isAuthenticated()");
+    }
+
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("mango");
+
+        return jwtAccessTokenConverter;
+    }
+}
